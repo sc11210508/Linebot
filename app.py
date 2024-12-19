930c2f25ae8a5005619672436669eb4aa4b054ae
sc11210508 <1121050008@gm.ntc.edu.tw>
2024-12-19 17:10:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ import os
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKE'))
+onfiguration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKE'))
 line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 # 用於記錄使用者狀態
